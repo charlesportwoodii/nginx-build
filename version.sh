2b6af1e76d35f0c90349445887cec19256fd0fca
Charles R. Portwood II <charlesportwoodii@erianna.com>
2020-04-22 12:06:31 -0500

    cut to ubuntu

---
+++ version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm .envs
+if [ -n "$DRONE_TAG" ]; then
+    echo "VERSION=$(cut -d '-' -f 1 <<< $DRONE_TAG)" | tee -a .envs
+    echo "RELEASEVER=$(cut -d '-' -f 2 <<< $DRONE_TAG) | tee -a .envs
+else
+    echo "VERSION=$DEFAULT_VERSION" | tee -a .envs
+    echo "RELEASEVER=$DEFAULT_REVISION" | tee -a .envs;
+fi
